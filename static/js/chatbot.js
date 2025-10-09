964c825e005d1a4f8da3f341446b36ab8eeadee8
lisekarimi <lisaka18121986@gmail.com>
2025-10-09 19:42:51 +0200

    fix: update primary color

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -4,7 +4,7 @@
     script.onload = function() {
         initializePageBotAI({
             chatbotName: 'MedokanjaBot',
-            primaryColor: '#895194',
+            primaryColor: '#a2129a',
             instructions: 'You are a helpful AI assistant that answers questions based on the content of the websites you can access. Be friendly, concise, and accurate in your responses. When mentioning documentation pages, always include the hash (#) in URLs, for example: https://medokanja.lisekarimi.com/docs/#/dax',
             targetUrls: ['https://medokanja.com/docs'],
             wsUrl: 'https://pagebotai.lisekarimi.com/ws/chat'
